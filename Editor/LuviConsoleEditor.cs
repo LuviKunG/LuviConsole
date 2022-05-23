77a926875cf86a9280cfafb80afee25ce7e43e5f
Thanut Panichyotai <luvikung@gmail.com>
2022-05-23 11:19:16 +0700

    update to version 2.5.1.

--- Editor/LuviConsoleEditor.cs
+++ Editor/LuviConsoleEditor.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 namespace LuviKunG.Console.Editor
 {
     [CustomEditor(typeof(LuviConsole))]
-    [CanEditMultipleObjects]
     public class LuviConsoleEditor : UnityEditor.Editor
     {
+        private const string LABEL_VERSION = "LuviConsole Version 2.5.1";
+        private const string LABEL_LINK = "https://github.com/LuviKunG/LuviConsole";
+        private const string LABEL_ASSIGN_KEY = "Please assign key to toggle LuviConsole.";
+        private const string LABEL_INFO_SWIPE = "Swipe ratio is disabled in non-target build mobile device.";
+
         private readonly GUIContent contentLogCapacity = new GUIContent("Log Capacity", "The capacity of list that will show debug log on console window.");
         private readonly GUIContent contentExecuteCapacity = new GUIContent("Log Excuted Command Capacity", "The capacity of excute command that have been excuted.");
         private readonly GUIContent contentSwipeRatio = new GUIContent("Swipe Ratio", "The ratio when you swipe up/down on your screen to toggle LuviDebug. 0 when touched, 1 when swipe entire of");
@@ -16,6 +20,7 @@ namespace LuviKunG.Console.Editor
         private readonly GUIContent contentAutoShowError = new GUIContent("Show Log When Error", "Automatically show logs when player is get error log.");
         private readonly GUIContent contentAutoShowException = new GUIContent("Show Log When Exception", "Automatically show logs when player is get exception log.");
         private readonly GUIContent contentCommandLog = new GUIContent("Log Command", "Log the command after you executed.");
+        private readonly GUIContent contentKeys = new GUIContent("Keys", "Keyboard keys to open the console.");
 
         private LuviConsole console;
         private StringBuilder sb;
@@ -27,6 +32,7 @@ namespace LuviKunG.Console.Editor
         private SerializedProperty autoShowError;
         private SerializedProperty autoShowException;
         private SerializedProperty commandLog;
+        private SerializedProperty keys;
 
         public void OnEnable()
         {
@@ -39,19 +45,13 @@ namespace LuviKunG.Console.Editor
             autoShowError = serializedObject.FindProperty(nameof(console.autoShowError));
             autoShowException = serializedObject.FindProperty(nameof(console.autoShowException));
             commandLog = serializedObject.FindProperty(nameof(console.commandLog));
+            keys = serializedObject.FindProperty(nameof(console.keys));
 
             sb = sb ?? new StringBuilder();
             sb.Clear();
-            sb.Append("LuviConsole Version 2.5.0");
+            sb.Append(LABEL_VERSION);
             sb.AppendLine();
-            sb.Append("https://github.com/LuviKunG/LuviConsole");
-            sb.AppendLine();
-            sb.AppendLine();
-#if UNITY_EDITOR_WIN
-            sb.Append("In editor, Press F1 for toggle LuviConsole.");
-#elif UNITY_EDITOR_OSX
-            sb.Append("In editor, Press Tap for toggle LuviConsole.");
-#endif
+            sb.Append(LABEL_LINK);
 #if UNITY_ANDROID
             sb.AppendLine();
             sb.Append("In your Android device, Swipe up direction in your screen for open LuviConsole. And swipe down for close LuviConsole.");
@@ -60,32 +60,39 @@ namespace LuviKunG.Console.Editor
             sb.Append("In your iPhone/iPad/iPod device, Swipe up direction in your screen for open LuviConsole. And swipe down for close LuviConsole.");
 #elif UNITY_WEBGL
             sb.AppendLine();
-            sb.Append("In your WebGL builds, Press Tap for toggle LuviConsole.");
+            sb.Append("In your WebGL builds, Use the same assigned button in editor.");
 #endif
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
+            EditorGUILayout.HelpBox(sb.ToString(), MessageType.None, true);
             using (var checkScope = new EditorGUI.ChangeCheckScope())
             {
                 logCapacity.intValue = EditorGUILayout.IntField(contentLogCapacity, logCapacity.intValue);
                 excuteCapacity.intValue = EditorGUILayout.IntField(contentExecuteCapacity, excuteCapacity.intValue);
 #if UNITY_ANDROID || UNITY_IOS
-                swipeRatio.floatValue = EditorGUILayout.Slider(contentSwipeRatio, swipeRatio.floatValue, 0f, 1f);
+                swipeRatio.floatValue = EditorGUILayout.Slider(contentSwipeRatio, swipeRatio.floatValue, 0.01f, 0.99f);
 #else
-                EditorGUILayout.HelpBox("Slider for swipe ratio is disabled.", MessageType.None, true);
+                EditorGUILayout.HelpBox(LABEL_INFO_SWIPE, MessageType.None, true);
 #endif
                 defaultFontSize.intValue = EditorGUILayout.IntSlider(contentDefaultFontSize, defaultFontSize.intValue, 8, 64);
                 autoShowWarning.boolValue = EditorGUILayout.Toggle(contentAutoShowWarning, autoShowWarning.boolValue);
                 autoShowError.boolValue = EditorGUILayout.Toggle(contentAutoShowError, autoShowError.boolValue);
                 autoShowException.boolValue = EditorGUILayout.Toggle(contentAutoShowException, autoShowException.boolValue);
                 commandLog.boolValue = EditorGUILayout.Toggle(contentCommandLog, commandLog.boolValue);
+                EditorGUILayout.PropertyField(keys);
+                if (!keys.isArray || keys.arraySize == 0)
+                {
+                    EditorGUILayout.HelpBox(LABEL_ASSIGN_KEY, MessageType.Error, true);
+                }
                 if (checkScope.changed)
+                {
                     EditorUtility.SetDirty(console);
+                    serializedObject.ApplyModifiedProperties();
+                }
             }
-            serializedObject.ApplyModifiedProperties();
-            EditorGUILayout.HelpBox(sb.ToString(), MessageType.Info);
         }
     }
 }
